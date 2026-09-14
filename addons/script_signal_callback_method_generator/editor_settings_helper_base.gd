62a576cf231f7dfb6d8e6b48bc32da583e2efbbc
airotto <171443091+airotto@users.noreply.github.com>
2026-09-15 01:37:42 +0900

    add description

--- addons/script_signal_callback_method_generator/editor_settings_helper_base.gd
+++ addons/script_signal_callback_method_generator/editor_settings_helper_base.gd
@@ -4,6 +4,14 @@ extends RefCounted
 var _plugin_name:String
 var _setting_property_list:Array[String]
 
+#{
+	#"plugin/setting_path":{
+		#"ja":"説明",
+		#"en":"description"
+	#}
+#}
+var _setting_description_list:Dictionary[String, Dictionary]
+
 ##これを継承先で使用してadd_settingをしてください。
 @abstract func _add_settings_initialized() -> void
 
@@ -11,9 +19,17 @@ var _setting_property_list:Array[String]
 func _init(p_plugin_name:String, plugin_node:EditorPlugin) -> void:
 	_plugin_name = p_plugin_name
 	
+	_description_system_initialized()
+	
 	_add_settings_initialized()
 	
-	add_setting("other/clear_editor_settings_when_disabling_the_plugin", false, TYPE_BOOL)
+	add_setting("other/clean_up_editor_settings_when_disabling_the_plugin", false, TYPE_BOOL)
+	set_description("other/clean_up_editor_settings_when_disabling_the_plugin", "en",
+"Cleans up editor settings when the plugin is deactivated
+(Use this to prevent settings from remaining even after you stop using the plugin.)")
+	set_description("other/clean_up_editor_settings_when_disabling_the_plugin", "ja",
+"プラグイン無効化時にエディター設定をクリーンアップします。
+(プラグインをもう使わない場合に設定が残り続けるのを防ぐためにこれを使用してください。)")
 	plugin_node.tree_exiting.connect(_on_plugin_tree_exiting)
 
 
@@ -57,7 +73,7 @@ func get_setting(property_name:String) -> Variant:
 
 
 func _on_plugin_tree_exiting() -> void:
-	if get_setting("other/clear_editor_settings_when_disabling_the_plugin"):
+	if get_setting("other/clean_up_editor_settings_when_disabling_the_plugin"):
 		_remove_setting_all()
 
 
@@ -70,3 +86,53 @@ func _remove_setting_all() -> void:
 
 func _get_plugin_settigns_dir() -> String:
 	return "plugins/" + _plugin_name + "/"
+
+
+func set_description(property_name:String, locale:String, description:String) -> void:
+	var property:String = _get_plugin_settigns_dir() + property_name
+	
+	if not _setting_description_list.has(property):
+		_setting_description_list[property] = {}# as Dictionary[String, Dictionary]
+	
+	if not _setting_description_list[property].has(locale):
+		_setting_description_list[property][locale] = {}# as Dictionary[String, String]
+	
+	_setting_description_list[property][locale] = description
+
+func code_block(text:String) -> String:
+	const CODEBLOCK_FONT_PATH:String = "res://addons/script_signal_callback_method_generator/codeblock_font.res"
+	if not ResourceLoader.exists(CODEBLOCK_FONT_PATH):
+		var codeblock_font := EditorInterface.get_editor_theme().get_font(&"font", &"CodeEdit")
+		ResourceSaver.save(codeblock_font, CODEBLOCK_FONT_PATH)
+	
+	return "[color=#d68f8f][bgcolor=#151515][font=" + CODEBLOCK_FONT_PATH + "]" + text + "[/font][/bgcolor][/color]"
+
+func _description_system_initialized() -> void:
+	var editor_settings_dialog:Node = EditorInterface.get_base_control().find_child("*EditorSettingsDialog*", false, false)
+	
+	EditorInterface.get_base_control().get_tree().node_added.connect(_on_node_added.bind(editor_settings_dialog))
+
+const EditorHelpBitToolTipHelper = preload("editor_help_bit_tool_tip_helper.gd")
+func _on_node_added(node: Node, editor_settings_dialog:Node) -> void:
+	if node is PopupPanel:
+		if node.get_class() == "EditorHelpBitTooltip":
+			if editor_settings_dialog.is_ancestor_of(node):
+				
+				if not node.is_node_ready():
+					await node.ready
+				
+				var tooltip_helper := EditorHelpBitToolTipHelper.new(node)
+				_on_tooltip_entered(tooltip_helper)
+
+func _on_tooltip_entered(tooltip_helper:EditorHelpBitToolTipHelper) -> void:
+	for property:String in _setting_description_list.keys():
+		if tooltip_helper.title_label.get_parsed_text().ends_with(property):
+			tooltip_helper.text_label.bbcode_enabled = true
+			
+			#var locale_description_list:Dictionary[String, String] = _setting_description_list[property]
+			var locale_description_list:Dictionary = _setting_description_list[property]
+			var description:String = locale_description_list["en"]
+			var locale:String = TranslationServer.get_locale()
+			if locale_description_list.has(locale):
+				description = locale_description_list[locale]
+			tooltip_helper.text_label.text = description
--- addons/script_signal_callback_method_generator/setting.gd
+++ addons/script_signal_callback_method_generator/setting.gd
@@ -1,6 +1,6 @@
 extends "editor_settings_helper_base.gd"
 
-
+##[code]ActionGenerateMode[/code]ActionGenerateMode
 enum ActionMode{
 	COPY, ##クリップボードにコピー
 	GENERATE, ##生成されます。位置については[code]ActionGenerateMode[/code]
@@ -12,26 +12,66 @@ enum ActionGenerateMode{
 
 
 func _add_settings_initialized() -> void:
-	##[code]action_mode[/code]が[code]ActionMode.GENERATE[/code]の時、生成時の上の関数との行間隔
+	
 	add_setting("line_space", 2, TYPE_INT, PROPERTY_HINT_RANGE, "0, 1, 1, or_greater")
-	##戻り型。空でも可
+	set_description("line_space", "en", "The line spacing between this function and the one above it during generation when "+ code_block("action_mode") + " is " + code_block("ActionMode.GENERATE") + ".")
+	set_description("line_space", "ja", code_block("action_mode") + "が" + code_block("ActionMode.GENERATE") + "の時、生成時の上の関数との行間隔。")
+	
 	add_setting("type", "void", TYPE_STRING)
+	set_description("type", "en", "Return type. May be left blank.")
+	set_description("type", "ja", "戻り型。空でも可。")
 	
 	add_setting("initial_text", "pass", TYPE_STRING)
-	##[code]action_mode[/code]が[code]ActionMode.GENERATE[/code]の時、生成時に自動で[code]initial_text[/code]を選択する
+	set_description("initial_text", "en", "The initial text within the function at the time of creation.")
+	set_description("initial_text", "ja", "生成時の関数内の初期テキスト。")
+	
 	add_setting("select_initial_text", true, TYPE_BOOL)
-	##connect()を作成する。[br][note][code]action_mode[/code]が何の場合でもこの設定は機能します。[/note]
+	set_description("select_initial_text", "en", "When "+ code_block("action_mode") + " is " + code_block("ActionMode.GENERATE") + "," + code_block("initial_text") + " is automatically selected during generation.")
+	set_description("select_initial_text", "ja", code_block("action_mode") + "が" + code_block("ActionMode.GENERATE") + "の時、生成時に自動で" + code_block("initial_text") + "を選択する。")
+	
 	add_setting("create_connect", true, TYPE_BOOL)
+	set_description("create_connect", "en", "Create a `connect()` function. [br][b]Note[/b]: This setting works regardless of the value of " + code_block("action_mode") +".")
+	set_description("create_connect", "ja", "connect()を作成する。[br][b]Note[/b]: " + code_block("action_mode") + "が何の場合でもこの設定は機能します。")
 	
 	add_setting("action_mode", ActionMode.find_key(ActionMode.GENERATE), TYPE_STRING, PROPERTY_HINT_ENUM, ",".join(ActionMode.keys()) )
+	set_description("action_mode", "en",
+"Types of Actions.
+
+[b]Copy[/b]: Copy the syntax to Clipboard.
+
+[b]Generate[/b]: Generate syntax.
+For information on the generation position, see " + code_block("action_generate_mode") )
+	set_description("action_mode", "ja",
+"アクションの種類。
+
+[b]Copy[/b]: クリッボードに構文をコピーします。
+
+[b]Generate[/b]: 構文を生成します。
+生成位置については"+ code_block("action_generate_mode") + "を参照")
 	
 	add_setting("action_generate_mode", ActionGenerateMode.find_key(ActionGenerateMode.NEXT), TYPE_STRING, PROPERTY_HINT_ENUM, ",".join(ActionGenerateMode.keys()) )
-	##connect()作成のみのアクションを新たに追加する
+	set_description("action_generate_mode", "en",
+"When " + code_block("action_mode") + "is set to " + code_block("ActionMode.GENERATE") + ", this determines where the syntax is generated.
+
+[b]Bottom[/b]: Generates the syntax at the very bottom.
+
+[b]Next[/b]: Generates the syntax at the next position where there is no indentation.")
+	set_description("action_generate_mode", "ja",
+code_block("action_mode") + "が" + code_block("ActionMode.GENERATE") + "の時、どこに構文を生成するか。
+
+[b]Bottom[/b]: 一番下に構文を生成します。
+
+[b]Next[/b]: 次にインデントが無い位置に構文を生成します。")
+	
 	add_setting("enable_connect_only_action", true, TYPE_BOOL)
-	##[code]action_mode[/code]が[code]ActionMode.GENERATE[/code]の時、すでにその名前の関数がある場合でも作成する
+	set_description("enable_connect_only_action", "en", "Add a new action that simply creates connect().")
+	set_description("enable_connect_only_action", "ja", "connect()作成のみのアクションを新たに追加する。")
+	
 	add_setting("force_generate", false, TYPE_BOOL)
-
-
+	set_description("force_generate", "en", "When "+ code_block("action_mode") + " is " + code_block("ActionMode.GENERATE") +", the function will be created even if one with that name already exists.")
+	set_description("force_generate", "ja", code_block("action_mode") + "が"+ code_block("ActionMode.GENERATE") + "の時、すでにその名前の関数がある場合でも作成する。")
+	
+	Resource.DeepDuplicateMode.DEEP_DUPLICATE_ALL
 
 
 func get_setting_line_space() -> int: return get_setting("line_space")
